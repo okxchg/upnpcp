eb1e6cbacd7f186376cc1ac5d7b15c6809dcaa01
Oliver Kindernay <oliver.kindernay@gmail.com>
2011-05-20 20:21:58 +0200

    Support for basic passive UPnP device discovery.

--- t/01-scan.t
+++ t/01-scan.t
@@ -1,3 +1,4 @@
+# This is pretty horribly indented
 #!/usr/bin/perl
 
 use strict;
@@ -6,8 +7,10 @@ use warnings;
 use lib '../';
 use upnpcp;
 
-use Test::More tests => 2;
+use Test::More tests => 10;
 use Test::Output;
+use Time::HiRes qw(time);
+use IO::Socket::INET;
 
 stdout_is { UPnPCP::run('scan', '-n') } "M-SEARCH * HTTP/1.1\r\n".
                                       "Host: 239.255.255.250:1900\r\n".
@@ -16,9 +19,99 @@ stdout_is { UPnPCP::run('scan', '-n') } "M-SEARCH * HTTP/1.1\r\n".
                                       "MX: 3\r\n\r\n";
 
 stdout_is { 
-    UPnPCP::run('scan', '-n', '-m', '40', '-b', 'target1') 
+    UPnPCP::run('scan', '-n', '-m', '40', '-b', 'target1', '-r', '1.1.1.1', '-p', '1234') 
 } "M-SEARCH * HTTP/1.1\r\n".
-  "Host: 239.255.255.250:1900\r\n".
+  "Host: 1.1.1.1:1234\r\n".
   "MAN: \"ssdp:discover\"\r\n".
   "ST: target1\r\n".
   "MX: 40\r\n\r\n";
+
+eval { UPnPCP::run('scan', '-a', '111.1.1.1111') };
+ok($@ =~ /^socket: Invalid argument/);
+
+eval { UPnPCP::run('scan', '-a', '1.1.1.1') };
+ok($@ =~ /^socket: Cannot assign requested address/);
+
+eval { UPnPCP::run('scan', '-r', '1.1.1.1111') };
+ok($@ =~ /^Bad arg length for Socket::pack_sockaddr_in/);
+
+my $time = time();
+UPnPCP::run('scan', '-t', '2');
+ok(int(time() - $time) == 2, 'timeout test');
+
+# This may contain race conditions but still better than nothing
+SKIP: {
+    skip 'This is not linux',1 if $^O ne 'linux';
+
+    use Socket;
+    use POSIX ":sys_wait_h";
+
+    my $ok = 0;
+    my $pid = fork();
+    if (!$pid) {
+        # change this ip of one of your interfaces
+        UPnPCP::run('scan', '-t', '3', '-a', '192.168.1.11', '-l', '1111');
+        exit 0;
+    } else {
+        sleep(1);
+        open(FILE, '/proc/net/udp') or die("Could not open /proc/net/udp: $!");
+
+        my @netstats = <FILE>;
+        shift @netstats;
+
+        for my $line (@netstats) {
+           my (undef, undef, $addr, $port) = split(/[:\s]+/, $line);
+           my $ip = inet_ntoa(pack("I<*", hex($addr)));
+           if ($ip eq '192.168.1.11' && hex($port) == 1111) {
+                $ok = 1;
+           }
+        }
+
+        waitpid($pid, 0);
+        ok($ok, 'bind test');
+    }
+}
+
+is_deeply(UPnPCP::parse_ssdp_response(
+<<EOH
+HTTP/1.1 200 OK\r
+ST:upnp:rootdevice\r
+Location: http://1.1.1.1:80/igd.xml\r
+Server: UPnP/1.0\r
+EXT:\r
+EOH
+), { 'HTTP/1.1 200 OK' => undef,
+     'ST' => 'upnp:rootdevice',
+     'LOCATION' => 'http://1.1.1.1:80/igd.xml',
+     'SERVER' => 'UPnP/1.0',
+     'EXT' => '',
+     'DATA' => ''
+}, 'parse_ssdp_response valid');
+
+# send no location header
+ok(!UPnPCP::parse_ssdp_response("HTTP/1.1 200 OK\r\n"), 
+   'parse_ssdp_response invalid');
+
+my $pid = fork();
+if (!$pid) {
+    sleep(1);
+    my $socket = IO::Socket::INET->new( 
+        Proto => 'udp', 
+        PeerAddr => 'localhost', 
+        PeerPort => 1900,
+        LocalAddr => "127.0.0.1",
+        LocalPort => 1234
+    ) or die("socket: $!");
+
+    print $socket <<EOH
+HTTP/1.1 200 OK\r
+Location: http://1.1.1.1:80/igd.xml\r
+Server: UPnP/1.0
+EOH
+    ;$socket->close;
+    exit 0;
+} else {
+    stdout_like { UPnPCP::run('scan', '-v', '-t', '3', '-l', '1900', '-a', '127.0.0.1') } 
+                qr|Device \d+ at http://1\.1\.1\.1:80/igd\.xml reported from 127\.0\.0\.1:1234| 
+}
+
--- upnpcp.pm
+++ upnpcp.pm
@@ -1,28 +1,31 @@
 #!/usr/bin/perl
 package UPnPCP;
 
-use Getopt::Std;
-
 use strict;
 use warnings;
 
+use Socket;
+use Getopt::Std;
+use IO::Socket::INET;
+use IO::Select;
+use Time::HiRes qw(time);
+use Data::Dumper;
+
 my $SSDP_MGROUP = '239.255.255.250';
 my $SSDP_RPORT = 1900;
+my $DEFAULT_TIMEOUT = 10;
 my $DEFAULT_MX = 3;
 my $DEFAULT_TARGET = 'upnp:rootdevice';
+my $MAX_RESPONSE_LEN = 9000;    
 
-my $scan_request = "M-SEARCH * HTTP/1.1\r\n".
-                    "Host: 239.255.255.250:1900\r\n".
-                    "MAN: \"ssdp:discover\"\r\n".
-                    "ST: <TARGET>\r\n".
-                    "MX: <MX>\r\n\r\n";
 my %actions = (
     'scan' => \&scan
 );
 
 my %action_parameters = (
-    'scan' => 't:m:b:r:p:n'
+    'scan' => 't:m:b:r:p:na:l:v'
 );
+
 
 run(@ARGV) unless caller();
 
@@ -46,7 +49,7 @@ ACTIONS:
 scan
 
 SCAN:
-    -t timeout          Timeout in seconds (10 is default). 0 means no timeout.
+    -t timeout          Timeout in seconds ($DEFAULT_TIMEOUT is default). 0 means no timeout.
                         This should be more than MX header value.
     -m timeout          SSDP MX header value. $DEFAULT_MX is default.
     -b target           Target to search for. '$DEFAULT_TARGET' is default.
@@ -54,7 +57,11 @@ SCAN:
                         multicast group '$SSDP_MGROUP', but you can also 
                         use unicast.
     -p port             Remote port on remote host. $SSDP_RPORT is default.
+    -a address          Bind to this address.
+    -l port             Bind to this port. This is mostly used for debugging 
+                        and testing.
     -n                  Don't send anything, just print ssdp request.
+    -v                  Print raw ssdp responses
 
 EOV
 }
@@ -62,23 +69,110 @@ EOV
 sub scan
 {
     my $opts = shift;
-    my $request = $scan_request;
 
-    # build request
     my $mx = $opts->{'m'} || $DEFAULT_MX;
     my $target = $opts->{'b'} || $DEFAULT_TARGET;
-    $request =~ s/<MX>/$mx/;
-    $request =~ s/<TARGET>/$target/;
+    my $remote_host = $opts->{'r'} || $SSDP_MGROUP;
+    my $rport = $opts->{'p'} || $SSDP_RPORT;
+
+    # build request
+    my $request = "M-SEARCH * HTTP/1.1\r\n".
+                  "Host: $remote_host:$rport\r\n".
+                  "MAN: \"ssdp:discover\"\r\n".
+                  "ST: $target\r\n".
+                  "MX: $mx\r\n\r\n";
 
     if (exists($opts->{'n'})) {
         print $request;
         return 0;
     }
+
+    # create socket and needed structures
+    my $search_socket;
+    my $response;
+    my $device_num = 0;
+    my $raddr = inet_aton($remote_host);
+    my $sockaddr = sockaddr_in($rport, $raddr);
+
+    $search_socket = IO::Socket::INET->new(
+        LocalAddr => $opts->{'a'},
+        Proto => 'udp',
+        LocalPort => $opts->{'l'} 
+    ) or die("socket: $!");
+
+    $search_socket->send($request, 0, $sockaddr) or die("send: $!");
+
+    my $select = IO::Select->new($search_socket) or die("select: $!");
+
+    my $time;
+    my $device_addr;
+    my $timeout;
+
+    if (defined($opts->{'t'})) {
+        $timeout = !$opts->{'t'} ? undef : $opts->{'t'};
+    } else {
+        $timeout = $DEFAULT_TIMEOUT;
+    }
+    
+    # We use select just for timeout. I think it's better to avoid 
+    # using SIGALRM as it can be undefined what happens with recv
+    # if it is interupted by signal.
+    $time = time;
+    while ($select->can_read($timeout)) {
+        $device_addr = $search_socket->recv($response, $MAX_RESPONSE_LEN)
+            or die("recv: $!");
+
+        my ($port,$ip_addr) = sockaddr_in($device_addr);
+
+        if ($opts->{'v'}) {
+            print $response;
+        }
+
+        my $headers = parse_ssdp_response($response);
+        if (!defined($headers)) {
+            print "Device $device_num from ".inet_ntoa($ip_addr).":".$port.
+                  " responded with invalid SSDP response\n";$|++;
+        } else { 
+            print "Device $device_num at $headers->{LOCATION} reported from ".
+                   inet_ntoa($ip_addr).":".$port."\n";$|++;
+        }
+
+        $device_num++;
+
+        if (defined($timeout)) {
+            $timeout = $timeout-(time()-$time);
+            $time = time();
+        }
+    }
 }
 
 sub parse_ssdp_response
+{   
+    # For now we don't care if response conforms with the UPnP standard, 
+    # we are just intrested in Location header. If it is present consider 
+    # response valid. This is subject to change in later releases.
+    my $headers = parse_http_headers(@_);
+    return undef if !$headers->{LOCATION};
+    return $headers;
+}
+
+sub parse_http_headers
 {
-     
+    my $raw_headers = shift;
+    my %parsed_headers;
+    my $i = 0;
+
+    my @headers = split("\r\n", $raw_headers);
+    for my $header(@headers) {
+        $i++;
+        last if (!length($header)); # End of headers
+        my ($name,$value) = split(/:\s*/, $header, 2);
+        $name = uc($name);
+        $parsed_headers{$name} = $value;
+    }
+    my $lastline = @headers-1;
+    $parsed_headers{DATA} = join("\n", @headers[$i..$lastline]);
+    return \%parsed_headers;
 }
 
 1;
